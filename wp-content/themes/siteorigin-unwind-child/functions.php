4c5da76a58f32ad999e87df5b49c26a08819c54b
Hiroyuki Narita <davidoff1125@gmail.com>
2018-03-13 22:57:55 +0900

    スタイルシートを上書きできないかテスト

--- wp-content/themes/siteorigin-unwind-child/functions.php
+++ wp-content/themes/siteorigin-unwind-child/functions.php
@@ -1,7 +1,10 @@
 <?php
+define( 'SITEORIGIN_THEME_VERSION', '1.2.10' );
+define( 'SITEORIGIN_THEME_CSS_PREFIX', '.min' );
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent`style', get_template_directory_uri() . '/style' . SITEORIGIN_THEME_CSS_PREFIX . '.css', array(), SITEORIGIN_THEME_VERSION );
+  // wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.min.css' );
   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
 );
 }
